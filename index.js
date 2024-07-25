b377c42e650de79c7305fde6dbeb51b227aef9c6
AdrielFerro-Dev <adrielferro.dev@gmail.com>
2024-07-25 10:43:05 -0300

    estruturando função no código

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 let classe = ""
 
-let vitoria = 30
+let vitoria = 599999
+let derrota = 10
+
+result()
+//fazer parte do código da função
+
+function result() {
+    result = vitoria - derrota
 
 
+    if (result <= 10) {
+        classe = "Ferro"
+    }
+    else if (result <= 20) {
+        classe = "Bronze"
+    }
+    else if (result <= 50) {
+        classe = "Prata"
+    }
+    else if (result <= 80) {
+        classe = "Ouro"
+    }
+    else if (result <= 90) {
+        classe = "Diamante"
+    }
+    else if (result <= 100) {
+        classe = "Lendário"
+    }
+    else {
+        classe = "Imortal"
+    }
 
-if(vitoria <= 10){
-    classe = "Ferro"
-}
-else if(vitoria <= 20){
-    classe = "Bronze"
-}
-else if( vitoria <= 50){
-    classe = "Prata"
+    console.log("o Herói tem saldo de " + result + " vitórias e está no nível " + classe)
 
 }
-else if(vitoria <= 80){
-    classe = "Ouro"
-}
-else if(vitoria <= 90){
-    classe = "Diamante"
-}
-else if(vitoria <= 100){
-    classe = "Lendário"
-}
-else{
-    classe = "Imortal"
-}
-
-console.log("o Herói tem saldo de "+ saldoVitorias + " está no nível de " + classe)
---
+++ indexTeste.js
@@ -0,0 +1,20 @@
+let vitoria = 15
+let derrota = 2
+
+Somar()
+
+function Somar() {
+    Somar = vitoria - derrota
+
+
+    if (Somar <= 5) {
+        console.log("Franguinha")
+    }
+    else if (Somar <= 10) {
+        console.log("Baaaum")
+    }
+    else {
+        console.log("agora vaaaai")
+    }
+}
+
